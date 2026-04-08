b2e8f715340cd71342b7572f0aecb79da02429fe
max-delvita <max@alphagamma.ai>
2026-04-08 22:51:37 +0800

    Style update

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/maxdelvita/Documents/ALPHA-PROJECTS/SYMPLEHOST/symple-host-astro/tina/__generated__/.cache/1775652303462', url: 'https://content.tinajs.io/2.2/content/eda83f0e-0c1d-4416-9977-bda7e06c23b4/github/main', token: '727463e228538cc82960dc01a9ad66e54b57d984', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '727463e228538cc82960dc01a9ad66e54b57d984', queries,  });
 export default client;
   
--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -624,7 +624,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "https://content.tinajs.io/2.2/content/eda83f0e-0c1d-4416-9977-bda7e06c23b4/github/main",
+        url: "http://localhost:4001/graphql",
         queries,
       })
     )
