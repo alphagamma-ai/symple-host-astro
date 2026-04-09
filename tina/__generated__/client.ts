36a4efab7c26d40c305e44172cd031ee5d15a2d8
Upasna1987 <upasna@theantmethod.com>
2026-04-09 17:43:02 +0800

    commit

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '727463e228538cc82960dc01a9ad66e54b57d984', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
 export default client;
   
